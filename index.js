33e51ca1883e597d7a2c6ba7de5c1e0f390a9aa5
Ian Davis <icampbelldavis@gmail.com>
2015-04-13 18:10:24 -0700

    amended index.js

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-var http = require('http');
+// var http = require('http');
 
-var Router = require("routes-router");
-var router = Router(); 
+// var Router = require("routes-router");
+// var router = Router(); 
 
-var st = require('st');
+// var st = require('st');
 
-// index.html can be handled in the same route!
-router.addRoute("/*", st({
-  path: __dirname + "/public",
-  // The 'index' property maps the route '/' onto '/public/index.html'
-  index:'/index.html'
-}));
+// // index.html can be handled in the same route!
+// router.addRoute("/*", st({
+//   path: __dirname + "/public",
+//   // The 'index' property maps the route '/' onto '/public/index.html'
+//   index:'/index.html'
+// }));
 
 
-var server = http.createServer(router);
-var port = process.argv[2] || 5000;
-console.log('server listening on port # '+port);
-server.listen(port);
+// var server = http.createServer(router);
+// var port = process.argv[2] || 5000;
+// console.log('server listening on port # '+port);
+// server.listen(port);
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
